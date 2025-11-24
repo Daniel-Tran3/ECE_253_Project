8affddebf794e46f59fd4e37f9a4a1802d7b76d8
Hsu <joh048@ieng6-202.ucsd.edu>
2025-11-23 23:25:34 -0800

    SFP Accum + RELU implementation and initializing corelet connections

--- implementations/daniel_corelet.v
+++ implementations/daniel_corelet.v
@@ -22,34 +22,46 @@ wire ofifo_full;
 wire l0_ready;
 wire l0_full;
 
-
+// MAC array
   mac_array #(.bw(bw), .psum_bw(psum_bw)) mac_array_instance (
     .clk(clk),
     .reset(reset),
-    .out_s(...),
+    .out_s(ofifo_input),    // output connected to SFU/OFIFO
     .in_w(l0_output), // I'm not sure if this is safe, or needs to be guarded by a control bit to make sure that l0_output is currently in weight loading mode.
     .in_n({psum_bw*col{1'b0}}),
-    .inst_w({inst_q[1], inst_q[0]}),
-    .valid(ofifo_wr)
+    .inst_w({inst_q[1], inst_q[0]}),  // instruction for MAC (kernel loading / execute)
+    .valid(ofifo_wr)    // output valid for each column
   );
 
+// L0 scratchpad (input activations)
   l0 #(.bw(bw), .row(row)) l0_instance (
     .clk(clk),
     .in(l0_input),
     .out(l0_output),
-    .rd(inst_q[3]),
-    .wr(inst_q[2]),
+    .rd(inst_q[3]),   // L0 read enable
+    .wr(inst_q[2]),   // L0 write enable
     .o_full(l0_full),
     .reset(reset),
     .o_ready(l0_ready)
   );
 
+// SFU: accumulate + relu
+  sfp #(.col(col), .psum_bw(psum_bw)) sfp_instance (
+      .clk(clk),
+      .reset(reset),
+      .in_psum(ofifo_input),    // MAC outputs
+      .valid_in(ofifo_wr),      // MAC output valid
+      .out_accum(ofifo_input),  // reuse ofifo_input as SFU output
+      .wr_ofifo(ofifo_wr),     // write enable for OFIFO
+      .o_valid(ofifo_valid)
+    );
+
   ofifo #(.col(col), .bw(psum_bw)) ofifo_instance (
     .clk(clk),
-    .in(ofifo_input),
+    .in(ofifo_input),   // SFU output
     .out(ofifo_output),
-    .rd(inst_q[6]),
-    .wr(ofifo_wr),
+    .rd(inst_q[6]),       // read enable
+    .wr(ofifo_wr),        // write enable from SFU
     .o_full(ofifo_full),
     .reset(reset),
     .o_ready(ofifo_ready),
